eee93d30cc79828f1fe2ae8b0489657f8001eeaa
MATEUS SCHEFFERm <mateusscheffer13@gmail.com>
2022-03-15 13:16:36 -0300

    1.5

--- js/typetest.js
+++ js/typetest.js
@@ -1,8 +1,15 @@
 import words from './words.js';
 
 let inputTemp = document.getElementById('input');
-let wordElement = document.getElementById('word')
+let wordElement = document.getElementById('word');
+let seconds = document.getElementById('sec');
+let prog = document.getElementById('prog');
+let wpm = document.getElementById('wpm');
+let prec = document.getElementById('prec');
 var correct = false;
+var hit = 1;
+var letterTyped = 1;
+var wordType = 1;
 
     getRandomWord();
 
@@ -12,35 +19,61 @@ function getRandomWord(){
     wordElement.innerText = words[Math.floor(Math.random() * 1952)]
 }
 
-function changeColor(color){
-    if(color =="red"){
-        document.getElementById('word').classList.add(color);
-        document.getElementById('word').classList.remove('green');
-    }else{
-        document.getElementById('word').classList.add(color);
+function isCorrect(condition){
+    if(condition =="yes"){
+        document.getElementById('word').classList.add('green');
         document.getElementById('word').classList.remove('red');
+    }else{
+        document.getElementById('word').classList.add('red');
+        document.getElementById('word').classList.remove('green');
     }
-    
 }
 
-window.checkword = checkword
-function checkword(){
+var sec = 30;
+
+window.startTimer = startTimer
+window.checkWord = checkWord
+ function startTimer(){
+    document.getElementById('img').classList.add('hidden')
+    document.getElementById('button').classList.add('hidden');
+    document.getElementById('end').classList.remove('hidden');
+    document.getElementById('info').classList.remove('hidden');
+    var x =  setInterval(function(){
+        seconds.textContent = sec
+        prog.value += 3.23;
+        sec--;
+        if(sec < 0){
+            clearInterval(x);
+        }
+    },1000)
+}
+
+function calculateInfo(){
+    prec.textContent = Math.round(hit*100/letterTyped) +"%"
+}
+
+function checkWord(){
+    if(inputTemp.value != ''){
+        console.log(letterTyped)
+        letterTyped++;
+    }
+    
     var input = inputTemp.value;
     var sliced = word.slice(0,input.length);
 
-    if(input == sliced.toLowerCase()){
+    if(input == sliced.toLowerCase()&& inputTemp.value != ''){
         correct = true;
-        changeColor('green');
+        hit++;
+        isCorrect('yes');
     }else{
         correct = false;
-        changeColor('red');
+        isCorrect('no');
     }
-
     if(input.length == word.length && correct){
         inputTemp.value = '';
         getRandomWord();
         word = document.getElementById('word').innerText;
         document.getElementById('word').classList.remove('green')
     }
+    calculateInfo();
 }
-
