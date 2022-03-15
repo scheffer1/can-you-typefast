d014e91a4d4dd8c8595c250aac76cb4a8579ebcc
MATEUS SCHEFFERm <mateusscheffer13@gmail.com>
2022-03-15 15:53:20 -0300

    Bug Fixes

--- js/typetest.js
+++ js/typetest.js
@@ -6,32 +6,46 @@ let seconds = document.getElementById('sec');
 let prog = document.getElementById('prog');
 let wpm = document.getElementById('wpm');
 let prec = document.getElementById('prec');
+
 var correct = false;
 var hit = 0;
 var letterTyped = 0;
 var wordTyped = 1;
 
-let word = document.getElementById('word').innerText;
 
-function getRandomWord(){
+let word = '';
+
+function getRandomWord() {
     wordElement.innerText = words[Math.floor(Math.random() * 1952)]
 }
 
-function isCorrect(condition){
-    if(condition =="yes"){
-        document.getElementById('word').classList.add('green');
-        document.getElementById('word').classList.remove('red');
-    }else{
-        document.getElementById('word').classList.add('red');
-        document.getElementById('word').classList.remove('green');
+function isCorrect(condition) {
+    switch(condition){
+        case 'yes':
+            document.getElementById('word').classList.add('green');
+            document.getElementById('word').classList.remove('red');
+            break;
+        case 'no':
+            document.getElementById('word').classList.add('red');
+            document.getElementById('word').classList.remove('green');
+            break;
+        case 'reset':
+            document.getElementById('word').classList.remove('green');
+            document.getElementById('word').classList.remove('red');
+            break;
     }
+        
+    
+        
+    
 }
 
 window.startTimer = startTimer
 window.checkWord = checkWord
 
-function startTimer(){
+function startTimer() {
     getRandomWord();
+    word = document.getElementById('word').innerText;
     var sec = 30;
     hit, letterTyped, wordTyped = 0;
     document.getElementById('img').classList.add('hidden')
@@ -41,58 +55,62 @@ function startTimer(){
     document.getElementById('finaltext').classList.add('hidden');
     document.getElementById('finalbutton').classList.add('hidden');
     resetInfo();
-    var x =  setInterval(function(){
-        seconds.textContent = sec+" seconds remaining"
+    var x = setInterval(function () {
+        seconds.textContent = sec + " seconds remaining"
         prog.value += 3.23;
         sec--;
-        if(sec < 0){
+        if (sec < 0) {
             endTimer();
             clearInterval(x);
         }
-    },1000)
+    }, 1000)
 }
 
-function endTimer(){
+function endTimer() {
     var decimal = wordTyped;
+    inputTemp.value = '';
     wpm.textContent = `WPM = ${Math.round(decimal * 100) / 100}`;
     document.getElementById('end').classList.add('hidden');
     document.getElementById('finaltext').classList.remove('hidden');
     document.getElementById('finalbutton').classList.remove('hidden');
 }
 
-function resetInfo(){
+function resetInfo() {
     prec.textContent = "Precision  0%";
     wpm.textContent = "WPM = 0";
     prog.value = 0;
 }
 
-function calculateInfo(){
-    prec.textContent = "Precision "+Math.round(hit*100/letterTyped) +"%"
+function calculateInfo() {
+    prec.textContent = "Precision " + Math.round(hit * 100 / letterTyped) + "%"
 }
 
-function checkWord(){
-    if(inputTemp.value != ''){
+function checkWord() {
+    if (inputTemp.value != '') {
         letterTyped++;
     }
-    
-    var input = inputTemp.value;
-    var sliced = word.slice(0,input.length);
 
-    if(input == sliced.toLowerCase()&& inputTemp.value != ''){
+    var input = inputTemp.value;
+    var sliced = word.slice(0, input.length);
+
+    console.log(input)
+    console.log(sliced.toLowerCase())
+    if (input == sliced.toLowerCase() && inputTemp.value != '') {
         correct = true;
         hit++;
         isCorrect('yes');
-    }else{
+    } else {
         correct = false;
         isCorrect('no');
     }
-    if(input.length == word.length && correct){
+    if (input.length == word.length && correct) {
         wordTyped++;
         console.log(wordTyped)
         inputTemp.value = '';
+        isCorrect('reset')
         getRandomWord();
         word = document.getElementById('word').innerText;
-        document.getElementById('word').classList.remove('green')
+        document.getElementById('word').classList.remove('green');
     }
     calculateInfo();
 }
